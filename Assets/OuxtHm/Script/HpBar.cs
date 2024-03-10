e8c76d657527ff87a727d647309a6932bc376cf4
OuxtHm <blue29856@naver.com>
2024-03-10 20:12:36 +0900

    Modifying HpBar Scripts
    체력바 애니메이션을 LateUpdate로 실행하는 것이 아닌 반복문으로 애니메이션을 실행

--- Assets/OuxtHm/Script/HpBar.cs
+++ Assets/OuxtHm/Script/HpBar.cs
@@ -12,7 +12,7 @@ public class HpBar : MonoBehaviour
     public float maxHp;   // 최대 체력
     public float curHp;   // 현재 체력
     public float hpRatio;       // 체력 비율
-
+    public float testCurHp;
     private void Awake()
     {
         hpBar = GetComponent<Image>();
@@ -22,20 +22,8 @@ public class HpBar : MonoBehaviour
     }
     void Start()
     {
-        hpRatio = curHp / maxHp;
-    }
-
-    private void LateUpdate()
-    {
         ShowHpText();
-        BringHp();
-        HandleHp();
-    }
-
-    void HandleHp()     // 체력바 애니메이션
-    {
-        hpRatio = Mathf.Lerp(hpRatio, curHp / maxHp, Time.deltaTime * 2);
-        hpBar.fillAmount = hpRatio;
+        hpRatio = curHp / maxHp;
     }
 
     void ShowHpText()       // 체력 상황을 텍스트로 표현
@@ -47,5 +35,24 @@ public class HpBar : MonoBehaviour
     {
         /*maxHp = player.playerMaxHp;
         curHp = player.playerCurHp;*/
+    }
+
+    public IEnumerator HandleHp()
+    {
+        while (Mathf.Abs(hpRatio - (curHp / maxHp)) >= 0.01f)
+        {
+            hpRatio = Mathf.Lerp(hpRatio, curHp / maxHp, Time.deltaTime * 2);
+            hpBar.fillAmount = hpRatio;
+            yield return null;
+        }
+        hpRatio = curHp / maxHp;
+        hpBar.fillAmount = hpRatio;
+    }
+
+    public void Test()      // 위의 코루틴을 실행하기 위한 버튼용 함수, 추후 데미지 및 포션 등에 활용가능
+    {
+        curHp = testCurHp;
+        ShowHpText();
+        StartCoroutine(HandleHp());
     }
 }
