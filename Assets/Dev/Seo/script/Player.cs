ae11135a0300357bcdb8aab48f83d25227a9144c
wjdgus0808 <1939wjdgus@naver.com>
2024-03-28 20:53:51 +0900

    palyer
    점프 좌우 이동 버그 수정 및 애니메이션 추가, 디자인 변경

--- Assets/Dev/Seo/script/Player.cs
+++ Assets/Dev/Seo/script/Player.cs
@@ -4,14 +4,25 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float playerMaxHP = 100f;  
+    public float playerHP = 100f;
+    public bool isBoosted = false; 
+    public float boostDuration = 0.1f;
+    public float boostedSpeed = 40f; // 증가된 이동 속도 값
+    public float originalSpeed; // 원래 이동 속도를 저장할 변수
     public static Player instance;
+    public bool isAttacking = false;
     public bool move;
     private Rigidbody2D rb;
     private Animator  animator;
     public float jumpForce = 250f;
-    public float moveSpeed = 2f;
+    public float moveSpeed = 4f;
     private SpriteRenderer sr;
-    //private bool isGrounded;
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask whatIsGround;
+    public bool isGround;
+    
 
     public void Awake()
     {
@@ -23,69 +34,91 @@ public class Player : MonoBehaviour
     }
     public void Start()
     {
-
+        originalSpeed = moveSpeed;
     }
 
     // Update is called once per frame
     public void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        //CheckGrounded();
+        isGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
 
-        if (Input.GetKeyDown(KeyCode.DownArrow))       
-        {         
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
             animator.SetBool("isCrawl", true);
         }
         else
-        {           
+        {
             animator.SetBool("isCrawl", false);
         }
 
-        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow))
+        if (isGround && (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow)))
         {
             animator.SetTrigger("isJump");
             rb.AddForce(new Vector2(0, jumpForce));
-            //isGrounded = false;
+            // isGround = false; //
         }
 
-        if (Input.GetKeyDown(KeyCode.A)) 
+        if (Input.GetKeyDown(KeyCode.A))
         {
             animator.SetTrigger("isAttack");
         }
 
-        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow))
-        {
-            move = true;
+        // 이동 상태 설정
+        move = horizontalInput != 0;
 
-            if (horizontalInput < 0)
-            {
-
-            }
-        }
-        // 왼쪽 화살표를 누르면 왼쪽으로 이동
-        else 
+        // 방향 전환 로직
+        if (horizontalInput < 0)
         {
-            move = false; 
-            if (horizontalInput < 0) 
-            {
-                sr.flipX = true;
-            }
-            else
-            {
-                sr.flipX = false;
-            }
+            sr.flipX = true; // 왼쪽을 바라보게 함
         }
+        else if (horizontalInput > 0)
+        {
+            sr.flipX = false; // 오른쪽을 바라보게 함
+        }   
 
         // Rigidbody의 속도 설정으로 이동 구현              
         Vector2 movement = new Vector2(horizontalInput, 0f) * moveSpeed * Time.deltaTime;
-
         transform.Translate(movement);
 
         // 이동 상태에 따라 애니메이터 변수 설정
         animator.SetBool("isRun", move);
-    }
-    /*private void CheckGrounded()
-    {
-        
-    }*/
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            StartCoroutine(BoostSpeedForDuration(boostDuration));
+            animator.SetTrigger("isDash");
+        }
+
+        IEnumerator BoostSpeedForDuration(float duration) // 새로 추가된 메소드
+        {
+            if (!isBoosted)
+            {
+                isBoosted = true;
+                moveSpeed = boostedSpeed; // 이동 속도를 증가시킴
+
+                yield return new WaitForSeconds(duration); // 일정 시간 대기
+
+                moveSpeed = originalSpeed; // 이동 속도를 원래대로 복원
+                isBoosted = false;
+            }
+        }
+
+        void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.collider.tag == "Enemy")
+            { 
+                playerHP -= 5; 
+                if (playerHP <= 0)
+                {
+                    animator.SetTrigger("isDie");
+                }
+                else if(playerHP > 0)
+                {
+                    animator.SetTrigger("isHit");
+                }
+            }
+        }
+    }           
 }
