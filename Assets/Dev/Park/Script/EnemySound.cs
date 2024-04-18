d530beed4b2c39ff2d8ab6425e0477b91bfa8cd7
pak0823 <ziwoo159487@gmail.com>
2024-04-18 19:47:46 +0900

    Enemy
    1스테이지 몬스터 애니메이션 일부 수정 및 사운드 추가

---
+++ Assets/Dev/Park/Script/EnemySound.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySound : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    [Header("1스테이지")]
+    public AudioClip slimeMove;
+    public AudioClip slimeDie;
+    public AudioClip canineAttack;
+    public AudioClip canineDie;
+    public AudioClip goblinAttack;
+    public AudioClip goblinDie;
+    public AudioClip frogAttack1;
+    public AudioClip frogIdle;
+    public AudioClip frogDie;
+    public AudioClip rabbitAttack;
+    public AudioClip rabbitDie;
+
+    //[Header("2스테이지")]
+
+
+    //[Header("3스테이지")]
+
+    private void Awake()
+    {
+        audioSource = this.gameObject.GetComponentInParent<AudioSource>();
+    }
+
+    public void SlimeMove()
+    {
+        Sounds("slimeMove");
+    }
+    public void SlimeDie()
+    {
+        Sounds("slimeDie");
+    }
+    public void CanineAttack()
+    {
+        Sounds("canineAttack");
+    }
+    public void CanineDie()
+    {
+        Sounds("canineDie");
+    }
+    public void GoblinAttack()
+    {
+        Sounds("goblinAttack");
+    }
+    public void GoblinDie()
+    {
+        Sounds("goblinDie");
+    }
+    public void FrogAttack1()
+    {
+        Sounds("frogAttack1");
+    }
+    public void FrogIdle()
+    {
+        Sounds("frogIdle");
+    }
+    public void FrogDie()
+    {
+        Sounds("frogDie");
+    }
+    public void RabbitAttack()
+    {
+        Sounds("rabbitAttack");
+    }
+    public void RabbitDie()
+    {
+        Sounds("rabbitDie");
+    }
+
+    public void Sounds(string sounds)
+    {
+        switch (sounds)
+        {
+            case "slimeMove":
+                audioSource.clip = slimeMove;
+                break;
+            case "slimeDie":
+                audioSource.clip = slimeDie;
+                break;
+            case "canineAttack":
+                audioSource.clip = canineAttack;
+                break;
+            case "canineDie":
+                audioSource.clip = canineDie;
+                break;
+            case "goblinAttack":
+                audioSource.clip = goblinAttack;
+                break;
+            case "goblinDie":
+                audioSource.clip = goblinDie;
+                break;
+            case "frogAttack1":
+                audioSource.clip = frogAttack1;
+                break;
+            case "frogIdle":
+                audioSource.clip = frogIdle;
+                break;
+            case "frogDie":
+                audioSource.clip = frogDie;
+                break;
+            case "rabbitAttack":
+                audioSource.clip = rabbitAttack;
+                break;
+            case "rabbitDie":
+                audioSource.clip = rabbitDie;
+                break;
+        }
+
+        audioSource.Play();
+    }
+}
