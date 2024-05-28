e7864e132e475b5dc836093246fd77b98213ad10
pak0823 <ziwoo159487@gmail.com>
2024-05-28 13:01:02 +0900

    Camera
    1-1, 1-2 카메라 이동 오브젝트 추가
    
    카메라 이동 스크립트 수정 중

--- Assets/Dev/Park/Script/Camera/CameraMove.cs
+++ Assets/Dev/Park/Script/Camera/CameraMove.cs
@@ -1,79 +1,148 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
-    public Player player;
-    public Camera camera;
-    public bool ismove = false;
-    public float Dir;
+    Player player;
+    Camera mainCamera;
+    public bool isMove = false;    //카메라 이동 가능 확인
+    float directionX;    // 플레이어가 지나간 X방향
+    float directionY;    // 플레이어가 지나간 Y방향
+    public int moveCount = 0;   //카메라가 더 이상 이동할 수 없는 위치인지 확인하는 변수 
+    int changeUp = 1;    //위 아래로 움직이는 카메라이동 오브젝트의 
+    int changeDown = 1;
 
     private void Awake()
     {
-        camera = GameObject.FindObjectOfType<Camera>();
+        mainCamera = Camera.main;
     }
 
-
-    void OnTriggerEnter2D(Collider2D collision)  //충돌 대미지 주기
+    void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision != null && collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        if (collision != null && (collision.gameObject.layer == LayerMask.NameToLayer("Player") || collision.gameObject.layer == LayerMask.NameToLayer("Dash")))
         {
-            player = collision.gameObject.GetComponent<Player>();
-            if (player != null)
+            player = collision.GetComponent<Player>();
+            if (player != null && !isMove)
             {
-                ismove = true;
+                isMove = true;
             }
             else
+            {
                 Debug.Log("플레이어를 못 불러옴");
+            }
+        }
+    }
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision != null && (collision.gameObject.layer == LayerMask.NameToLayer("Player") || collision.gameObject.layer == LayerMask.NameToLayer("Dash")))
+        {
+            player = collision.GetComponent<Player>();
+            if (player != null && !isMove)
+            {
+                isMove = true;
+            }
+            else
+            {
+                Debug.Log("플레이어를 못 불러옴");
+            }
         }
     }
 
     void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision != null && collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        if (collision != null && (collision.gameObject.layer == LayerMask.NameToLayer("Player") || collision.gameObject.layer == LayerMask.NameToLayer("Dash")))
         {
-            player = collision.gameObject.GetComponent<Player>();
+            player = collision.GetComponent<Player>();
             if (player != null)
             {
-                Dir = this.transform.position.x - player.transform.position.x;
-                if(ismove)
+                directionX = transform.position.x - player.transform.position.x;
+                directionY = transform.position.y - player.transform.position.y;
+                if (isMove)
                 {
-                    if (this.tag == "CameraDown")
-                        camera.transform.position = new Vector3(camera.transform.position.x, -2, camera.transform.position.z);
-                    else if(this.tag == "CameraUp")
-                        camera.transform.position = new Vector3(camera.transform.position.x, 4, camera.transform.position.z);
+                    if (tag == "CameraDown")
+                    {
+                        if(changeDown == 1)
+                            mainCamera.transform.position = new Vector3(mainCamera.transform.position.x, mainCamera.transform.position.y - 5f, mainCamera.transform.position.z);
+                        else
+                            mainCamera.transform.position = new Vector3(mainCamera.transform.position.x, mainCamera.transform.position.y + 5f, mainCamera.transform.position.z);
+
+                        changeDown *= -1;
+                    }
+                    else if (tag == "CameraUp")
+                    {
+                        
+                        if (changeUp == 1)
+                            mainCamera.transform.position = new Vector3(mainCamera.transform.position.x, mainCamera.transform.position.y + 5f, mainCamera.transform.position.z);
+                        else
+                            mainCamera.transform.position = new Vector3(mainCamera.transform.position.x, mainCamera.transform.position.y - 5f, mainCamera.transform.position.z);
+
+                        changeUp *= -1;
+                    }
+
                     MovingCamera();
                 }
-                
             }
             else
+            {
                 Debug.Log("플레이어를 못 불러옴");
+            }
         }
     }
 
-
-    IEnumerator MoveCameraToNextPos(float targetX)
+    IEnumerator MoveCameraToNextPosX(float targetX)
     {
-        float moveSpeed = 18; // 카메라 이동속도
-        float duration = Mathf.Abs((targetX - camera.transform.position.x) / moveSpeed) * Time.deltaTime;
-        Vector3 targetPosition = new Vector3(targetX, camera.transform.position.y, camera.transform.position.z);
-
-        while (Vector3.Distance(camera.transform.position, targetPosition) > 0.01f)
+        BoxCollider2D boxCollider = GetComponent<BoxCollider2D>();
+        float moveSpeed = 18;
+        float duration = Mathf.Abs((targetX - mainCamera.transform.position.x) / moveSpeed) * Time.deltaTime;
+        Vector3 targetPositionX = new Vector3(targetX, mainCamera.transform.position.y, mainCamera.transform.position.z);
+        boxCollider.isTrigger = false;
+        while (Vector3.Distance(mainCamera.transform.position, targetPositionX) > 0.01f)
         {
-            camera.transform.position = Vector3.MoveTowards(camera.transform.position, targetPosition, moveSpeed * Time.deltaTime);
+            mainCamera.transform.position = Vector3.MoveTowards(mainCamera.transform.position, targetPositionX, moveSpeed * Time.deltaTime);
             yield return null;
         }
 
-        ismove = false;
+        isMove = false;
+        yield return new WaitForSeconds(1f);
+        boxCollider.isTrigger = true;
+        
+    }
+
+    IEnumerator MoveCameraToNextPosY(float targetY)
+    {
+        BoxCollider2D boxCollider = GetComponent<BoxCollider2D>();
+        float moveSpeed = 18;
+        float duration = Mathf.Abs((targetY - mainCamera.transform.position.y) / moveSpeed) * Time.deltaTime;
+        Vector3 targetPositionY = new Vector3(mainCamera.transform.position.x, targetY, mainCamera.transform.position.z);
+        boxCollider.isTrigger = false;
+        while (Vector3.Distance(mainCamera.transform.position, targetPositionY) > 0.01f)
+        {
+            mainCamera.transform.position = Vector3.MoveTowards(mainCamera.transform.position, targetPositionY, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+
+        isMove = false;
+        yield return new WaitForSeconds(1f);
+        boxCollider.isTrigger = true;
+
     }
 
     void MovingCamera()
     {
-        float moveDistance = 24;    //카메라 사이즈에 맞게 설정한 이동거리임
-        float nextPos = camera.transform.position.x + (Dir < 0 ? moveDistance : -moveDistance);
+        moveCount += directionX < 0 ? 1 : -1;
+        //YmoveCount += directionY < 0 ? 1 : -1;
 
-        StartCoroutine(MoveCameraToNextPos(nextPos));
+        if (moveCount >= 0)
+        {
+            float moveDistanceX = 20;
+            float moveDistanceY = 10;
+            float nextPosX = mainCamera.transform.position.x + (directionX < 0 ? moveDistanceX : -moveDistanceX);
+            float nextPosY = mainCamera.transform.position.y + (directionY < 0 ? moveDistanceY : -moveDistanceY);
+            StartCoroutine(MoveCameraToNextPosX(nextPosX));
+        }
+        else
+        {
+            moveCount = 0;
+        }
     }
-
 }
--- Assets/Dev/Seo/script/Player.cs
+++ Assets/Dev/Seo/script/Player.cs
@@ -237,7 +237,7 @@ public class Player : MonoBehaviour
         {
             StartCoroutine(BoostSpeedForDuration(boostDuration, lastHorizontalInput));
             animator.SetTrigger("isDash");
-            gameObject.layer = LayerMask.NameToLayer("Enemy");
+            gameObject.layer = LayerMask.NameToLayer("Dash");
             //canChangeDirDuringDash = false;
         }
 
